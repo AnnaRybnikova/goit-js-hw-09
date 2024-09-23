483894ff0699d7f0ef1dc6ad9ea8317f524b274a
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2024-09-23 23:56:47 +0200

    task-2 done

---
+++ src/js/2-form.js
@@ -0,0 +1,36 @@
+const FEEDBACK_STORAGE_KEY = "feedback-form-state";
+let formData = {
+    email: "",
+    message: ""
+};
+
+const formEl = document.querySelector('.feedback-form');
+const savedData = localStorage.getItem(FEEDBACK_STORAGE_KEY);
+
+if (savedData) {
+    formData = JSON.parse(savedData);
+    formEl.email.value = formData.email || '';
+    formEl.message.value = formData.message || '';
+}
+
+formEl.addEventListener("input", event => {
+  formData[event.target.name] = event.target.value.trim();
+  localStorage.setItem(FEEDBACK_STORAGE_KEY, JSON.stringify(formData));
+});
+
+formEl.addEventListener("submit", event => {
+    event.preventDefault();
+
+    if (!formEl.email.value || !formEl.message.value) {
+        window.alert('Fill please all fields');
+    }
+    else {
+        console.log(formData);
+    }
+
+    localStorage.removeItem(FEEDBACK_STORAGE_KEY);
+    formData.email = "";
+    formData.message = "";
+    formEl.email.value = "";
+    formEl.message.value = "";
+})
